6450fe03d4112fd679fa5f955114d5ecb68c9c69
Aaron Gonzales <38227777+aarong31@users.noreply.github.com>
2020-01-08 22:43:37 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -16,15 +16,19 @@ var commentRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes 		= require("./routes/index")
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp"
+mongoose.connect(url, {
 useUnifiedTopology: true,
 useNewUrlParser: true});
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 //seedDB(); //seed the database
+app.locals.moment = require('moment');
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
